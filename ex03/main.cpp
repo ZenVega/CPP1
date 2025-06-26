e5d6babd77d5e41b627ddb3dec524ba9b337d07e
Urs Schmidt <ufogreeks@gmail.com>
2025-06-26 11:17:50 +0200

    feat: setup ex03

---
+++ ex03/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: uschmidt <uschmidt@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/06 16:28:11 by uschmidt          #+#    #+#             */
+/*   Updated: 2025/06/25 15:46:06 by uschmidt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/main.hpp"
+
+int main()
+{
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+	return 0;
+}
